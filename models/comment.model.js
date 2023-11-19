857d2b0a899b61fa6fd00d7ce810369ccc3c2f75
kavania2002 <kavan155gondalia@gmail.com>
2023-11-19 16:36:50 +0530

    update: add comment model

--- models/comment.model.js
+++ models/comment.model.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose')
 
-const commendSchema = new mongoose.Schema({
+const commentSchema = new mongoose.Schema({
     commentedOn: { type: mongoose.Types.ObjectId },
     commentedBy: { type: mongoose.Types.ObjectId },
     postId: { type: mongoose.Types.ObjectId },
-    
+    description: String
 }, {
     timestamps: {
         createdAt: "created_at",
         updatedAt: "updated_at"
     }
-}
-)
+})
+
+module.exports = new mongoose.model("Comment", commentSchema)
