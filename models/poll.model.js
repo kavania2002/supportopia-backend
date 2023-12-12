e7480a64cd95bd141a964a399f396e65a6a8c96a
Kavan Gondalia <kavan155gondalia@gmail.com>
2023-12-12 06:24:39 +0530

    update: show percentage in polls

--- models/poll.model.js
+++ models/poll.model.js
@@ -1,14 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const pollSchema = new mongoose.Schema({
-    creatorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    question: String,
-    options: [String],
-    votes: [
-        [
-            { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-        ]
-    ]
-})
+  creatorId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  question: String,
+  totalVotes: { type: Number, default: 0 },
+  options: [String],
+  votes: [[{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]],
+  percentage: [],
+});
 
-module.exports = new mongoose.model("Poll", pollSchema)
+module.exports = new mongoose.model("Poll", pollSchema);
--- services/poll.service.js
+++ services/poll.service.js
@@ -6,8 +6,10 @@ const pollService = {
   addNewPoll: async (userId, data) => {
     const numberOfOptions = data.options.length;
     let votes = [];
+    let percentage = [];
     for (let number = 0; number < numberOfOptions; number++) {
       votes.push([]);
+      percentage.push(0);
     }
 
     try {
@@ -16,6 +18,8 @@ const pollService = {
         question: data.questions,
         options: data.options,
         votes: votes,
+        percentage,
+        totalVotes: 0,
       });
 
       await newPoll.save();
@@ -39,8 +43,16 @@ const pollService = {
       const pollFound = await Poll.findById(data.pollId);
 
       pollFound.votes[data.option].push(userId);
-      await pollFound.save();
 
+      pollFound.totalVotes = pollFound.totalVotes + 1;
+
+      let percentage = [];
+      pollFound.votes.forEach((voters) => {
+        percentage.push(voters.length / pollFound.totalVotes);
+      });
+
+      pollFound.percentage = percentage;
+      await pollFound.save();
       return {
         data: pollFound,
         message: "Vote Added",
